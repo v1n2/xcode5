5ca549b95ed2dcd203cb9a3c00cce5f4462a55e7
v1n2 <v1n2@v1n2s-rMBP.local>
2014-08-27 14:29:44 +0200

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by v1n2 on 27.08.14.
+//  Copyright (c) 2014 OmegaSeven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  test
+//
+//  Created by v1n2 on 27.08.14.
+//  Copyright (c) 2014 OmegaSeven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
